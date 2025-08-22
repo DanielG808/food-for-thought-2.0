883cb635e58c075a2a8eec864898d57b996ab3a9
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 14:27:02 -0400

    added reusable input-or-textarea component

--- web/src/components/auth-form.tsx
+++ web/src/components/auth-form.tsx
@@ -1,9 +1,12 @@
 import Form from "./ui/form";
+import InputOrTextarea from "./ui/input-or-textarea";
 
 export default function AuthForm() {
   return (
     <div className="flex flex-1 justify-center items-center">
-      <Form>Form</Form>
+      <Form>
+        <InputOrTextarea />
+      </Form>
     </div>
   );
 }
---
+++ web/src/components/ui/input-or-textarea.tsx
@@ -0,0 +1,3 @@
+export default function InputOrTextarea() {
+  return <div>InputOrTextarea</div>;
+}
