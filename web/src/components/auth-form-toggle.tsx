dad65b14d29e64466377c6c74408eb837920eeae
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 15:37:30 -0400

    added functionality to auth-form-toggle

--- web/src/components/auth-form-toggle.tsx
+++ web/src/components/auth-form-toggle.tsx
@@ -1,8 +1,22 @@
-export default function AuthFormToggle() {
+type AuthFormToggleProps = {
+  formType: "sign-in" | "sign-up";
+  toggleForm: () => void;
+};
+
+export default function AuthFormToggle({
+  formType,
+  toggleForm,
+}: AuthFormToggleProps) {
+  const message =
+    formType === "sign-in" ? "Don't have an account?" : "Already signed up?";
+  const buttonText = formType === "sign-in" ? "Sign Up" : "Sign In";
+
   return (
     <div className="flex items-center justify-between w-full text-sm pt-1">
-      <p>Don&apos;t have an account?</p>
-      <button className="cursor-pointer hover:underline">Sign Up</button>
+      <p>{message}</p>
+      <button onClick={toggleForm} className="cursor-pointer hover:underline">
+        {buttonText}
+      </button>
     </div>
   );
 }
--- web/src/components/auth-form.tsx
+++ web/src/components/auth-form.tsx
@@ -1,15 +1,24 @@
+"use client";
+
+import { useState } from "react";
 import AuthFormToggle from "./auth-form-toggle";
 import SignInForm from "./sign-in-form";
 import Form from "./ui/form";
 
 export default function AuthForm() {
+  const [formType, setFormType] = useState<"sign-in" | "sign-up">("sign-in");
+
+  function toggleForm() {
+    setFormType((prev) => (prev === "sign-in" ? "sign-up" : "sign-in"));
+  }
+
   return (
     <div className="flex flex-col justify-center space-y-2">
       <h1 className="text-lg text-foreground/45 font-medium">Sign In:</h1>
       <Form className="border border-foreground-dark/15 w-96 bg-background-dark/85 p-4 space-y-1 rounded-md">
         <SignInForm />
       </Form>
-      <AuthFormToggle />
+      <AuthFormToggle formType={formType} toggleForm={toggleForm} />
     </div>
   );
 }
