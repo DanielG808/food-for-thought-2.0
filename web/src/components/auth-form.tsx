f568c097c6d3ef34730a0ec83ea49765c6ee75bf
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 14:23:06 -0400

    removed sign-in-link from sign-in-page

--- web/src/components/auth-form.tsx
+++ web/src/components/auth-form.tsx
@@ -1,5 +1,3 @@
 export default function AuthForm() {
-  return (
-    <div className="flex flex-1 justify-center items-center">AuthForm</div>
-  );
+  return <div className="flex flex-1 justify-center items-center"></div>;
 }
