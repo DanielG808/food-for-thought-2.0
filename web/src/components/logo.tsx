d0744fd86228666d67e4296e99d0383732b303ac
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 12:38:55 -0400

    made logo a homepage link

--- web/src/components/logo.tsx
+++ web/src/components/logo.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Logo() {
   return (
-    <h1 className="font-pacifico text-5xl hover:text-foreground-dark hover:underline">
-      Food For Thought 2.0
-    </h1>
+    <Link href="/">
+      <h1 className="font-pacifico text-5xl cursor-pointer hover:text-foreground-dark hover:underline duration-200">
+        Food For Thought 2.0
+      </h1>
+    </Link>
   );
 }
