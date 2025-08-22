5405b985907637eb6049ef76ed17c15cbe05fddf
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 14:53:28 -0400

    added styles to input-or-textarea component

--- web/src/components/auth-form.tsx
+++ web/src/components/auth-form.tsx
@@ -3,9 +3,9 @@ import InputOrTextarea from "./ui/input-or-textarea";
 
 export default function AuthForm() {
   return (
-    <div className="flex flex-1 justify-center items-center">
-      <Form>
-        <InputOrTextarea />
+    <div className="flex justify-center items-center">
+      <Form className="border border-foreground-dark/15 w-80 bg-background-dark/85 p-4 space-y-2 rounded-md">
+        <InputOrTextarea name="username" />
       </Form>
     </div>
   );
--- web/src/components/ui/input-or-textarea.tsx
+++ web/src/components/ui/input-or-textarea.tsx
@@ -1,3 +1,29 @@
-export default function InputOrTextarea() {
-  return <div>InputOrTextarea</div>;
+import { cn } from "@/lib/utils/cn";
+
+type InputOrTextareaProps = {
+  type?: "input" | "textarea";
+  name: string;
+  className?: string;
+};
+
+export default function InputOrTextarea({
+  type = "input",
+  name,
+  className,
+}: InputOrTextareaProps) {
+  const baseStyles =
+    "w-full bg-background text-black/75 p-1 focus:ring-1 focus:ring-foreground-dark/15 focus:outline-0 rounded-md";
+  const label = name.charAt(0).toUpperCase() + name.slice(1);
+
+  return type === "input" ? (
+    <div className="flex flex-col space-y-1">
+      <label htmlFor={name}>{`${label}:`}</label>
+      <input id={name} type="text" className={cn(baseStyles, className)} />
+    </div>
+  ) : (
+    <div className="flex flex-col space-y-1">
+      <label htmlFor={name}>{`${label}:`}</label>
+      <textarea name="" id="" className={cn(baseStyles, className)} />
+    </div>
+  );
 }
