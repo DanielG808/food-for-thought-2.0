6c7f6475ce10f0e1b9c41d1c755d99882f59e63f
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 14:18:30 -0400

    changed footer bg color

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -2,7 +2,7 @@ import CopyrightStatement from "./copyright-statement";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center text-center text-black/20 mt-auto h-14">
+    <footer className="flex justify-center items-center text-center text-black/20 bg-footer mt-auto h-14">
       <CopyrightStatement />
     </footer>
   );
