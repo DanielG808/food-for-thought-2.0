4be3b376c80e3f749a45a0869a996cdd044abb06
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 12:43:46 -0400

    added link to sign-in-link and styled link text

--- web/src/components/sign-in-link.tsx
+++ web/src/components/sign-in-link.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function SignInLink() {
-  return <div>SignInLink</div>;
+  return (
+    <Link href="/sign-in" className="pr-2 hover:underline duration-200">
+      Sign In
+    </Link>
+  );
 }
