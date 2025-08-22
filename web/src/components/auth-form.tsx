4621ed7be35e3c04cd76f91f17657dffc4d9120b
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 14:25:25 -0400

    added reusable form component

--- web/src/components/auth-form.tsx
+++ web/src/components/auth-form.tsx
@@ -1,3 +1,9 @@
+import Form from "./ui/form";
+
 export default function AuthForm() {
-  return <div className="flex flex-1 justify-center items-center"></div>;
+  return (
+    <div className="flex flex-1 justify-center items-center">
+      <Form>Form</Form>
+    </div>
+  );
 }
---
+++ web/src/components/ui/form.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@/lib/utils/cn";
+
+type FormProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function Form({ children, className }: FormProps) {
+  return <form className={cn("flex flex-col", className)}>{children}</form>;
+}
