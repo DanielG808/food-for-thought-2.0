8996bbc7edd0b307d8e9e2c7c3a906d4d1e451a1
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 11:43:51 -0400

    configured base color styles and styled logo

--- web/src/components/logo.tsx
+++ web/src/components/logo.tsx
@@ -1,3 +1,7 @@
 export default function Logo() {
-  return <h1 className="font-pacifico text-5xl">Food For Thought 2.0</h1>;
+  return (
+    <h1 className="font-pacifico text-5xl hover:text-foreground-dark hover:underline">
+      Food For Thought 2.0
+    </h1>
+  );
 }
