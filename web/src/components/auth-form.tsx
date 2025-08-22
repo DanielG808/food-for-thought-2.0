abb2e2296d8964201e8c329a5acf541824b184f7
Dan G <daniel.w.gray1994@gmail.com>
2025-08-22 15:57:26 -0400

    created reusable form-header component

--- web/src/components/auth-form.tsx
+++ web/src/components/auth-form.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import AuthFormToggle from "./auth-form-toggle";
-import SignInForm from "./sign-in-form";
 import Form from "./ui/form";
+import FormHeader from "./ui/form-header";
 import SignUpForm from "./sign-up-form";
+import SignInForm from "./sign-in-form";
+import AuthFormToggle from "./auth-form-toggle";
+
 import { useAuthFormToggle } from "@/lib/hooks/useAuthFormToggle";
 
 export default function AuthForm() {
@@ -11,9 +13,7 @@ export default function AuthForm() {
 
   return (
     <div className="flex flex-col justify-center space-y-2">
-      <h1 className="text-lg text-foreground/45 font-medium">
-        {formType === "sign-in" ? "Sign In:" : "Sign Up:"}
-      </h1>
+      <FormHeader text={formType === "sign-in" ? "Sign In:" : "Sign Up:"} />
       <Form className="border border-foreground-dark/15 w-96 bg-background-dark/85 p-4 space-y-1 rounded-md">
         {formType === "sign-in" ? <SignInForm /> : <SignUpForm />}
       </Form>
