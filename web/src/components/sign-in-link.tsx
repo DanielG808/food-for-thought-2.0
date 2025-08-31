71463f317d239d03ba1b85d0b52433cdd10f3f8b
Dan G <daniel.w.gray1994@gmail.com>
2025-08-31 16:08:00 -0400

    conditionally rendered sign-in-link when user is not signed in

--- web/src/components/sign-in-link.tsx
+++ web/src/components/sign-in-link.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useAuth } from "@clerk/nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function SignInLink() {
+  const { isSignedIn } = useAuth();
   const pathname = usePathname();
 
+  if (isSignedIn) return null;
   if (pathname === "/sign-in") return null;
 
   return (
