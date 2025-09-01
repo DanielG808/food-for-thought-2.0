384ae5f43c990b301316abd6aa71267c93946dfa
Dan Gray <daniel.w.gray1994@gmail.com>
2025-09-01 16:16:50 -0400

    adjusted layout styling

--- web/src/components/recipe-card-container.tsx
+++ web/src/components/recipe-card-container.tsx
@@ -9,7 +9,7 @@ export default function RecipeCardContainer({
   recipes,
 }: RecipeCardContainerProps) {
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {recipes.map((recipe) => (
         <li key={recipe.id}>
           <RecipeCard recipe={recipe} />
--- web/src/components/recipe-card.tsx
+++ web/src/components/recipe-card.tsx
@@ -5,5 +5,12 @@ type RecipeCardProps = {
 };
 
 export default function RecipeCard({ recipe }: RecipeCardProps) {
-  return <div>{recipe.title}</div>;
+  return (
+    <article className="border border-foreground-dark/15 py-2 px-3 rounded-md w-full">
+      <h1 className="text-xl font-medium cursor-pointer hover:text-foreground-dark hover:underline duration-100">
+        {recipe.title}
+      </h1>
+      <p className="text-sm">{recipe.description}</p>
+    </article>
+  );
 }
