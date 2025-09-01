31a6178b5b909f2e5210ca9b92c95de2e0207f38
Dan Gray <daniel.w.gray1994@gmail.com>
2025-09-01 18:09:30 -0400

    added uniform button styling

--- web/src/components/recipe-card.tsx
+++ web/src/components/recipe-card.tsx
@@ -6,20 +6,20 @@ type RecipeCardProps = {
 
 export default function RecipeCard({ recipe }: RecipeCardProps) {
   return (
-    <article className="group flex flex-col border border-foreground-dark/15 py-2 px-3 rounded-md w-full h-44 cursor-pointer hover:bg-background-dark/45">
+    <article className="group flex flex-col border border-foreground-dark/15 py-2 px-3 rounded-md w-full h-44 cursor-pointer hover:bg-background-dark/45 hover:shadow-sm duration-150">
       <div>
-        <h1 className="text-xl font-medium group-hover:text-foreground-dark group-hover:underline duration-100">
+        <h1 className="text-xl font-medium group-hover:text-foreground-dark group-hover:underline duration-150">
           {recipe.title}
         </h1>
-        <p className="pt-2 text-sm group-hover:text-foreground-dark/75">
+        <p className="pt-2 text-sm group-hover:text-foreground-dark/75 duration-150">
           {recipe.description}
         </p>
       </div>
 
-      <div className="mt-auto flex justify-between text-sm text-foreground/45 pt-3 group-hover:text-foreground-dark/55">
+      <div className="mt-auto flex justify-between text-sm text-foreground/45 pt-3 group-hover:text-foreground-dark/55 duration-150">
         <p>
           Created by{" "}
-          <span className="text-foreground/75 group-hover:text-foreground-dark/75">
+          <span className="text-foreground/75 group-hover:text-foreground-dark/75 duration-150">
             {recipe.user.username}
           </span>
         </p>
