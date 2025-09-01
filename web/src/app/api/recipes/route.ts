2d8800064a61192b04419e35b157f4d3482d1074
Dan Gray <daniel.w.gray1994@gmail.com>
2025-09-01 14:01:58 -0400

    implemented POST req at /api/recipes

--- web/src/app/api/recipes/route.ts
+++ web/src/app/api/recipes/route.ts
@@ -1,5 +1,10 @@
 import { getAllRecipes } from "@/lib/api/recipes";
+import { recipeCreateSchema } from "@/lib/validations/recipeSchema";
+import { auth } from "@clerk/nextjs/server";
+import { prisma } from "@fft/db";
+import { error } from "console";
 import { NextResponse } from "next/server";
+import { da } from "zod/locales";
 
 export async function GET() {
   try {
@@ -8,5 +13,45 @@ export async function GET() {
   } catch (error) {
     console.error("GET /api/recipes failed:", error);
     return new NextResponse("Failed to fetch recipes", { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const { userId: clerkUserId } = await auth();
+    if (!clerkUserId) return new NextResponse("Unauthorized.", { status: 401 });
+
+    const user = await prisma.user.findUnique({
+      where: { clerkId: clerkUserId },
+      select: { id: true },
+    });
+    if (!user) return new NextResponse("User not found.", { status: 401 });
+
+    const json = await req.json();
+    const parsed = recipeCreateSchema.safeParse(json);
+    if (!parsed.success) {
+      return NextResponse.json(
+        { errors: parsed.error.flatten() },
+        { status: 400 }
+      );
+    }
+
+    const data = parsed.data;
+    const recipe = await prisma.recipe.create({
+      data: {
+        userId: user.id,
+        title: data.title,
+        description: data.description ?? null,
+        ingredients: data.ingredients,
+        body: { steps: data.body.steps },
+        prepTime: data.prepTime ?? 0,
+        cookTime: data.cookTime ?? 0,
+      },
+    });
+
+    return NextResponse.json({ recipe }, { status: 201 });
+  } catch (error) {
+    console.error("POST /api/recipes failed:", error);
+    return new NextResponse("Failed to create recipe.", { status: 500 });
   }
 }
