64f0facbc84cef9f82791cb1406b0b53059d20e2
Dan Gray <daniel.w.gray1994@gmail.com>
2025-09-02 18:37:06 -0400

    changed getAllRecipes to getRecipesPage and modified function to support pagination

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,17 +1,25 @@
 import PageContentWrapper from "../components/page-content-wrapper";
 import Paginator from "../components/paginator";
 import RecipeCardContainer from "../components/recipe-card-container";
-import { getAllRecipes } from "../lib/api/recipes/recipes";
+import { getRecipesPage } from "../lib/api/recipes/recipes";
+import { PER_PAGE } from "../lib/constants/recipesPerPageLimit";
 
-export default async function Home() {
-  const recipes = await getAllRecipes();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: { page?: string; perPage?: string };
+}) {
+  const page = Math.max(1, Number(searchParams?.page ?? 1));
+  const perPage = Number(searchParams?.perPage ?? PER_PAGE);
 
-  console.log(recipes);
+  const { items, totalPages } = await getRecipesPage({ page, perPage });
+
+  console.log(items);
 
   return (
     <PageContentWrapper className="flex flex-col items-center w-4/5 mx-auto my-10">
-      <RecipeCardContainer recipes={recipes} />
-      <Paginator currentPage={1} totalPages={12} />
+      <RecipeCardContainer recipes={items} />
+      <Paginator currentPage={page} totalPages={totalPages} />
     </PageContentWrapper>
   );
 }
