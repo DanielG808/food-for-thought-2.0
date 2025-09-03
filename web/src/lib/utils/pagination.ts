c97f479a20cfa8245658c6094c7afef08f76f42d
Dan Gray <daniel.w.gray1994@gmail.com>
2025-09-03 14:51:53 -0400

    imported just UrlObject type to make the utils function server side only

--- web/src/lib/utils/pagination.ts
+++ web/src/lib/utils/pagination.ts
@@ -1,5 +1,5 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import { UrlObject } from "url";
+import { type UrlObject } from "url";
 
 export type PageToken = number | "...";
 
