e702e281e79113b69d8f3090a2893c5439c9547d
Dan G <daniel.w.gray1994@gmail.com>
2025-09-05 15:49:45 -0400

    styled recipe info container

--- web/src/app/recipes/page.tsx
+++ web/src/app/recipes/page.tsx
@@ -11,7 +11,7 @@ export default function RecipesPage({ searchParams }: MyRecipesPageProps) {
   const recipeId = searchParams.recipeId;
 
   return (
-    <PageContentWrapper>
+    <PageContentWrapper className="flex flex-col w-3/5 border-l border-r border-black/5 mx-auto shadow-[inset_8px_0_8px_-8px_rgba(0,0,0,0.3),inset_-8px_0_8px_-8px_rgba(0,0,0,0.3)]">
       <UserRecipesContainer userId={userId} />
       <RecipePageContainer recipeId={recipeId} />
     </PageContentWrapper>
