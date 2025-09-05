a87ee6bbc2a93c0182daa0db7efa8f0ceaae3e69
Dan G <daniel.w.gray1994@gmail.com>
2025-09-05 15:58:34 -0400

    added header to recipe by id page

--- web/src/components/recipe-page-container.tsx
+++ web/src/components/recipe-page-container.tsx
@@ -1,4 +1,5 @@
 import { getRecipeById } from "../lib/api/recipes/getRecipeById";
+import Button from "./ui/button";
 
 type RecipePageContainerProps = {
   recipeId: string | undefined;
@@ -11,5 +12,19 @@ export default async function RecipePageContainer({
 
   const recipe = await getRecipeById(recipeId);
 
-  return recipe && <h1>{recipe.title}</h1>;
+  return (
+    recipe && (
+      <section className="p-10">
+        <header className="flex justify-between">
+          <div className="flex flex-col w-[600px] space-y-4">
+            <h1 className="text-3xl">{recipe.title}</h1>
+            <h2 className="text-xl text-foreground/50">{recipe.description}</h2>
+          </div>
+          <div className="flex items-end">
+            <Button className="h-10">Share</Button>
+          </div>
+        </header>
+      </section>
+    )
+  );
 }
