51bc9d807cca8c8d371ae135fdc007430a20516b
Dan G <daniel.w.gray1994@gmail.com>
2025-09-05 20:28:07 -0400

    changed icon and text for recipe shuffler

--- web/src/components/refresh-button.tsx
+++ web/src/components/refresh-button.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { RefreshCcw } from "lucide-react";
+import { Shuffle } from "lucide-react";
 import Button from "./ui/button";
 import { useRouter } from "next/navigation";
 
 export default function RefreshButton() {
   const router = useRouter();
   return (
-    <Button onClick={() => router.refresh()} className="flex space-x-1">
-      <RefreshCcw className="size-5" />
-      <span>Refresh</span>
+    <Button onClick={() => router.refresh()} className="gap-x-1">
+      <Shuffle className="size-5" />
+      <span>Shuffle recipes</span>
     </Button>
   );
 }
