e64e7a66bf1374ba59fcf2e3fe7f9afbf3f3e4eb
Dan G <daniel.w.gray1994@gmail.com>
2025-09-05 20:16:44 -0400

    added functionality to refresh-button

--- web/src/components/refresh-button.tsx
+++ web/src/components/refresh-button.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { RefreshCcw } from "lucide-react";
 import Button from "./ui/button";
+import { useRouter } from "next/navigation";
 
 export default function RefreshButton() {
+  const router = useRouter();
   return (
-    <Button className="flex space-x-1">
+    <Button onClick={() => router.refresh()} className="flex space-x-1">
       <RefreshCcw className="size-5" />
       <span>Refresh</span>
     </Button>
